25073b08889050e3786f50f6ba002b369c5a0651
Sheelabhan <shilubhandari23@gmail.com>
2021-02-23 14:20:22 +0530

    I have dome a quotation requested check which goes direct to the home page.

--- app/src/main/java/com/sheela/neonproject/quotationReqActivity.java
+++ app/src/main/java/com/sheela/neonproject/quotationReqActivity.java
@@ -2,13 +2,24 @@ package com.sheela.neonproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class quotationReqActivity extends AppCompatActivity {
-
+     Button btnquotation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation_req);
+        btnquotation=findViewById(R.id.btnquotation);
+        btnquotation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(quotationReqActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sheela/neonproject/sucessfulQuotationActivity.java
@@ -0,0 +1,14 @@
+package com.sheela.neonproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sucessfulQuotationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucessful_quotation);
+    }
+}
