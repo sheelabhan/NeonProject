2ac44ef9d80d95f0d79ada277a64bffa703203ec
Sheelabhan <shilubhandari23@gmail.com>
2021-02-24 16:11:03 +0530

    I have done a categories Activity

--- app/src/main/java/com/sheela/neonproject/sucessfulQuotationActivity.java
+++ app/src/main/java/com/sheela/neonproject/sucessfulQuotationActivity.java
@@ -3,12 +3,21 @@ package com.sheela.neonproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sucessfulQuotationActivity extends AppCompatActivity {
-
+     Button btnhome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sucessful_quotation);
+        btnhome=findViewById(R.id.btnhome);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
