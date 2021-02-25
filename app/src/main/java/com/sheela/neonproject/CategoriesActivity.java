8deb00d69d988d5050f8734401edba44215f9839
Sheelabhan <shilubhandari23@gmail.com>
2021-02-25 13:37:10 +0530

    I have done a cartImageActivity

--- app/src/main/java/com/sheela/neonproject/CategoriesActivity.java
+++ app/src/main/java/com/sheela/neonproject/CategoriesActivity.java
@@ -2,13 +2,25 @@ package com.sheela.neonproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CategoriesActivity extends AppCompatActivity {
-
+   Button btncategories, btnbrands;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        btncategories= findViewById(R.id.btncategories);
+        btnbrands=findViewById(R.id.btnbrands);
+        btnbrands.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(CategoriesActivity.this, CartImageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
